af257bf2eddf518c043e7c1dbc4207f0d6185277
thayron <devthayronhiglander@gmail.com>
2025-03-29 09:40:42 -0300

    Salvando dados no banco de dados

--- project/settings.py
+++ project/settings.py
@@ -126,6 +126,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# uploud de arquivos 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
